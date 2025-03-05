3dc88c95753d3c3910fec3529e27af8c9d7a0605
BoraEkiz <Boraekiz70@gmail.com>
2025-03-05 23:46:57 +0300

    .

---
+++ src/çinzodya.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class çinzodya {
+    public static void main(String[] args) {
+
+        String[] hayvan = {
+                "Maymun", "Horoz", "Köpek", "Domuz", "Fare",
+                "Öküz", "Kaplan", "Tavşan", "Ejderha", "Yılan",
+                "At", "Koyun"
+        };
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Doğum yılınız nedir: ");
+        int dogumYili = scanner.nextInt();
+
+        int index = dogumYili % 12;
+
+        System.out.println("Çin Zodyağı burcunuz: " + hayvan[index]);
+
+
+    }
+}
